cb7f88f20817eaea920b1b855a92ea66a432bd7a
Colin-Stark <collinscodes@gmail.com>
2024-10-19 16:01:40 -0400

    added vercel and route to test

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 require('dotenv').config();
+const dalle = require('./routes/dalleGeneration');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/promptImage', dalle);
 
 const PORT = process.env.port;
 
